8f026129ad7cb9652ceecc248c20e1956d80f5a5
Sudip Tamang <sudeep.tmg62@gmail.com>
2026-06-20 22:55:24 +0545

    learn about condition statement

--- basic.py
+++ basic.py
@@ -5,10 +5,40 @@
 
 #variable
 
-age = 12
-name = "Hello"
+# age = 12
+# name = "Hello"
 
-price = 200.50
+# price = 200.50
 
-print(f"Your age is {age} sdfsdf {price}" )
+# is_student = False
+
+# print(f"Are you a student {is_student}")
+
+
+# print(f"Your age is {age} sdfsdf {price}" )
+
+
+#conditional statements
+
+# is_good_boy = True
+# age = 10
+
+# if age>2 and  is_good_boy:
+#     print("Yes")
+
+# else:
+#     print("No bad ")
+
  
+
+# marks = float(input("Enter your marks"))
+
+# if marks > 80.5:
+#     print("Outstanding")
+# elif marks >60:
+#     print("Excellent")
+# else:
+#     print("failed")
+
+
+
