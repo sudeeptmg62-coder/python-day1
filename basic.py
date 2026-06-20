0e077ae3abdf70a16d742d3191c12c3e7e09ef94
Sudip Tamang <sudeep.tmg62@gmail.com>
2026-06-20 22:41:15 +0545

    learned about variable

--- basic.py
+++ basic.py
@@ -1,4 +1,14 @@
 
-print("Hello world")  
-print("Hello world 2")  
+# print("Hello world")  
+# print("Hello world 2")  
 
+
+#variable
+
+age = 12
+name = "Hello"
+
+price = 200.50
+
+print(f"Your age is {age} sdfsdf {price}" )
+ 
